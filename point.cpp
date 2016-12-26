9fe9b7b2f1be9d7eaa37c7d3b723fd1bce6018eb
ILISI2 <ilisi2.github@gmail.com>
2016-12-26 09:09:53 +0000

    create point.cpp

---
+++ point.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "point.h"
+
+
+point::point(int xx, int yy):x(xx),y(yy)
+{
+}
+
+
+point::~point()
+{
+}
+
+bool point::operator<(const point &) const
+{
+	return (x<y);
+}
+
+bool point::operator>(const point & p) const
+{
+	return x > p.x;
+}
+
+bool point::operator!=(const point & p) const
+{
+	bool r = (x != p.x || y != p.y);
+	return r;
+}
+
+bool point::operator==(const point & p) const
+{
+	
+	return !((*this)!=p);
+}
+
+ostream & operator<<(ostream & s, const point & p)
+{
+	// TODO: insert return statement here
+	s << p.x << " " << p.y << " ";
+	return s;
+}
